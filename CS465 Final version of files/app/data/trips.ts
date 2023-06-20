672885eb2e02764a67779fd396de277e2cd22005
SaltyWalty22 <107817833+SaltyWalty22@users.noreply.github.com>
2023-06-20 12:55:06 -0400

    Files Added
    Due to full project being too big to upload, adding final versions of most Docs

---
+++ CS465 Final version of files/app/data/trips.ts
@@ -0,0 +1,37 @@
+export const trips = 
+[
+     {
+      "code":"GALR210215:",   
+      "name": "Gale Reef",
+      "length" : "4 nights / 5 days",
+      "start": "2021-02-14T08:00:00Z",
+      "resort": "Emerald Bar, 3 stars",
+      "perPerson": "799.00",
+      "image": "reef1.jpg",
+      "description": "<p>Gale Reef is working as it should be."
+      },
+      {
+          "code":"DAWR210315:",
+          "name": "Dawson Reef",
+                
+          "length" : "4 nights / 5 days",
+          "start": "2021-03-15T08:00:00Z",
+          "resort": "Blue Lagoon, 4 stars",
+          "perPerson": "1199.00",
+          
+          "image": "reef2.jpg",
+          "description": "<p>Dawson Reef is Sed et augue lorem."
+      }, 
+       {
+          "code":"CLAR210621:",
+          "name": "Claire's Reef",
+  
+          "length" : "4 nights / 5 days",
+          "start": "2021-06-21T08:00:00Z",
+          "resort": "Coral Sands, 4 stars",
+          "perPerson": "1999.00",
+  
+          "image": "reef3.jpg",
+          "description": "<p>Claire Reef is Sed et augue lorem."
+      }
+    ]
